9c36ebfcfb1c58bef0bc0e8a8b54a88b891fa079
H4rryp0tt3r <nagesh.podilapu@thoughtworks.com>
2016-09-11 15:15:40 +0530

    [Nagesh] Remove leading zero when user starts typing & Add π operation to calculator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -3,10 +3,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    private var userInTheMiddleOfTyping = false
 
     @IBAction func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        print("Digit Pressed: \(digit)")
+
+        if userInTheMiddleOfTyping {
+            let textInCurrentDisplay = display.text!
+            display.text = textInCurrentDisplay + digit
+        } else {
+            display.text = digit
+        }
+
+        userInTheMiddleOfTyping = true
+    }
+
+    @IBAction func performOperation(sender: UIButton) {
+        if let mathSymbol = sender.currentTitle {
+            if mathSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
 
     override func viewDidLoad() {
